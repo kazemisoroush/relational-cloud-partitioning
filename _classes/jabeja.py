6580a111d43c774edf1ec9a9658794091c80fa9c
Soroush Kazemi <kazemi.soroush@gmaill.com>
2016-07-01 17:03:37 +0430

    adds jabeja main class.

---
+++ _classes/jabeja.py
@@ -0,0 +1,18 @@
+__author__ = 'Soroush'
+
+
+class JabeJa(object):
+    def __init__(self):
+        """
+        :return:
+        """
+        pass
+
+    def partition(self, graph, partitions):
+        """
+        TODO: Main partitioning algorithm.
+        :param graph:
+        :param partitions:
+        :return:
+        """
+        pass
